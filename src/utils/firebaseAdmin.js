934318de431ac9d3a11a7c4915a0ecc823abcf60
chandu <chandugit68@gmail.com>
2025-12-26 22:05:54 +0530

    moved the firebase secrets into env file

--- src/utils/firebaseAdmin.js
+++ src/utils/firebaseAdmin.js
@@ -1,8 +1,13 @@
-const admin=require('firebase-admin')
-const serviceAccount=require('../../serviceAccount.json')
+const admin = require('firebase-admin')
+// const serviceAccount=require('../../serviceAccount.json')
+try {
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+    });
+    console.log("firebase connected")
+} catch (err) {
+    console.log(err)
+}
 
-admin.initializeApp({
-    credential:admin.credential.cert(serviceAccount),
-});
-
-module.exports=admin;
+module.exports = admin;
