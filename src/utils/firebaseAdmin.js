a6694a84735618742b440d772b8543353b90f983
chandu <chandugit68@gmail.com>
2025-12-26 22:32:42 +0530

    firebase added to env

--- src/utils/firebaseAdmin.js
+++ src/utils/firebaseAdmin.js
@@ -1,7 +1,10 @@
 const admin = require('firebase-admin')
 // const serviceAccount=require('../../serviceAccount.json')
 try {
-    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+    const decoded = Buffer
+        .from(process.env.FIREBASE_SERVICE_ACCOUNT_B64, "base64")
+        .toString("utf8");
+    const serviceAccount = JSON.parse(decoded)
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
     });
