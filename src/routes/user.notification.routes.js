bc011e25fc9a98eaaf500257e39d87c1e3a1f871
chandu <chandugit68@gmail.com>
2026-01-26 15:34:06 +0530

    bug fixed in notification routes

--- src/routes/user.notification.routes.js
+++ src/routes/user.notification.routes.js
@@ -4,8 +4,8 @@ const { getNotifications, markAllAsRead, markAsRead } = require('../controllers/
 const checkAuth = require('../middlewares/auth.middleware')
 
 notificationRouter.get('/get/notifications', checkAuth, getNotifications);
-notificationRouter.patch('/mark/:id/read', checkAuth, markAsRead);
 notificationRouter.patch('/mark/all/read', checkAuth, markAllAsRead);
+notificationRouter.patch('/mark/:id/read', checkAuth, markAsRead);
 
 module.exports = notificationRouter;
 
